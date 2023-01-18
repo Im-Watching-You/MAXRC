7b8cb5dc2257afc270736d550b56e35258f97de2
Im-Watching-You <maksym.labs@gmail.com>
2023-01-18 14:24:53 +0900

    Feature B

--- main.py
+++ main.py
@@ -22,19 +22,39 @@ if __name__ == '__main__':
 	cv2.destroyAllWindows()
 
 
-# dsds A
-#                                 1983148141	mp4
 # >>> cv2.cv.FOURCC( *"XVID" )    1145656920	avi
 # >>> cv2.cv.FOURCC( *"MJPG" )    1196444237	avi
 
 
-# # Connect a GitHub connection into VSCode
-# $ git config --list --show-origin
-# $ git config --global user.name "Im-Watching-You"
-# $ git config --global user.email "maksym.labs@gmail.com"
-# $ git config --global core.editor "subl -n -w"
+# Connect a GitHub connection into VSCode
+	# $ git config --list --show-origin
+	# $ git config --global user.name "Im-Watching-You"
+	# $ git config --global user.email "maksym.labs@gmail.com"
+	# $ git config --global core.editor "subl -n -w"
 
-# $ git config --global init.defaultBranch main
+	# $ git config --global init.defaultBranch main
+
+
+# MAIN push code:
+	# git clone git@github.com/Im-Watching-You/GitHub.git
+	# git add .
+	# git status
+	# git commit -m "First commit"
+	# git push
+
+# MAIN pull code:
+	# git pull
+
+# MAIN create branch code:
+	# git checkout -b feature_A
+	# max-height-integration
+
+# git branch -d <local-branch>
+# git push origin --delete <remote-branch-name>
+
+# $ git branch <branch_name>   # create
+# $ git checkout <branch_name> # switch to <branch_name>
+# $ git branch -a # show all
 
 
 
@@ -42,6 +62,7 @@ if __name__ == '__main__':
 # git add .
 # git status
 # git commit -m "First commit"
+
 # 	git reset HEAD~1
 # git remote add origin git@github.com/Im-Watching-You/GitHub-test.git
 # git remote -v
@@ -49,10 +70,21 @@ if __name__ == '__main__':
 # git diff
 # git checkout .
 
-# git clone git@github.com/Im-Watching-You/GitHub.git
-# git add .
-# git status
-# git commit -m "First commit"
-# git push
 
-# git pull
+# github@branch/c/remote/push  (new-branch)
+# git clone https://github.com/learn-git-fast/git-branch-examples.git
+# cd git*
+# git checkout -b new-branch
+
+# github@branch/c/remote/push (new-branch)
+# git branch -a
+# touch devolution.jpg
+# git add .
+# git commit -m "Are we not gender neutral people? We are Devo?"
+# git push --set-upstream origin feature_A
+
+# github@branch/c/remote/push (new-branch)
+# touch eden.html
+# git add .
+# git commit -m "Eden added"
+# git push origin
